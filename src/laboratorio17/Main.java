595dedb6a641cef7c7eeafd05835ead76d1e1005
Gustavo <glinares@unsa.edu.pe>
2024-11-10 15:05:15 -0500

    Clase Main completa

--- src/laboratorio17/Main.java
+++ src/laboratorio17/Main.java
@@ -8,6 +8,12 @@ package laboratorio17;
  *
  * @author Gustavo
  */
+import java.util.Random;
 public class Main {
-    
+    public static void main(String[] args) {
+        String[] tiposDeTerritorio = {"Bosque", "Campo Abierto", "Montana", "Desierto", "Playa"};
+        Random random = new Random();
+        String tipoSeleccionado = tiposDeTerritorio[random.nextInt(tiposDeTerritorio.length)];
+        Mapa mapa = new Mapa(tipoSeleccionado);
+    }
 }
